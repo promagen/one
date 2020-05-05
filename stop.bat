2d9d9734d9c472fa9c5a48ebc8e8239fb30ffb75
tom <tom@sapletta.com>
2020-05-05 15:52:48 +0200

    update

--- stop.bat
+++ stop.bat
@@ -3,4 +3,5 @@ netstat -a -n -o | findstr ":3001"
 FOR /F "tokens=5" %%T IN ('netstat -a -n -o ^| findstr ":3001" ') DO (
 echo ProcessId to kill = %%T
 taskkill /pid %%T /F /T
-wmic process where "ParentProcessId=%%T" delete)
+wmic process where "ParentProcessId=%%T" delete
+exit)

21c3f38a8af2e62583e2d3e56da36f0d50467edb
tom <tom@sapletta.com>
2020-05-08 12:04:42 +0200

    docs

--- start.bat
+++ start.bat
@@ -1,2 +1,8 @@
-@echo off
-node server.js
+echo I will start NODEJS serverfor this project ...
+WHERE node
+IF %ERRORLEVEL% NEQ 0 (
+    echo nodejs is not installed on this system!
+) else (
+    node server.js
+)
+

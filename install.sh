d20acaa03f8ca7e23918a804b75b4b35c2410581
tom <tom@sapletta.com>
2020-05-17 19:12:42 +0200

    install script

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 echo "I will install NPM packages on this project ..."
+project_root=$(dirname $(dirname $(dirname $(realpath $0 ))))
+echo $project_root
+
+data_dir="$project_root/examples/data"
+echo "DATA: $data_dir"
+
 which npm
 if [ $? -eq 0 ]
 then

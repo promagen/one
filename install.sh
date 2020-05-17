11e76606c3134a4319008f08989c33b2bb4b310d
tom <tom@sapletta.com>
2020-05-17 19:31:04 +0200

    install script

--- install.sh
+++ install.sh
@@ -17,3 +17,13 @@ else
   echo "npm is not installed on this system!" >&2
   exit 1
 fi
+
+#CODE NO
+#1 - Catchall for general errors
+#2 - Misuse of shell builtins (according to Bash documentation)
+#126 - Command invoked cannot execute
+#127 - “command not found”
+#128 - Invalid argument to exit
+#128+n - Fatal error signal “n”
+#130 - Script terminated by Control-C
+#255\* - Exit status out of range

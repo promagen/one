ec9acc489bf333ed3504ad4a8545c5673baaa29d
tom <tom@sapletta.com>
2020-05-17 19:56:15 +0200

    install script

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-full_path=$(realpath $0)
-dir_path=$(dirname $full_path)
-echo $dir_path
-node $dir_path/server.js
+#full_path=$(realpath $0)
+#dir_path=$(dirname $full_path)
+#echo $dir_path
+#node $dir_path/server.js
+node server.js

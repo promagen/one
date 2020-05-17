1b881443065617c5b583d7ec4d8b82fd34da100e
tom <tom@sapletta.com>
2020-05-17 19:19:28 +0200

    install script

--- install.sh
+++ install.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 echo "I will install NPM packages on this project ..."
-echo $0
 
 full_path=$(realpath $0)
 echo $full_path
 
-dir_path=$(dirname $full_path)
-echo $dir_path
-
-examples=$(dirname $dir_path )
-echo $examples
-
 which npm
 if [ $? -eq 0 ]
 then
-  ls -la
-  npm install
+  ls -d $full_path
+  npm install --prefix $full_path
 #echo "The script ran ok"
   exit 0
 else

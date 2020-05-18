7b3a5c1f9f053539ea0f5eef3bbb94c275916332
tom <tom@sapletta.com>
2020-05-18 07:05:31 +0200

    update logs about projects

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,8 @@ var localConfig = new PublicConfig();
 var projectList = require('../src/project_list');
 var title = 'ProMaGen One';
 
+console.log("projectList.private",projectList.private);
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {title: title, projects: projectList.public, projects_private: projectList.private, testurl: localConfig.testurl});
